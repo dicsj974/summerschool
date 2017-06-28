4f82ae61f7cdabce6dcf0ba9313f26967474e173
dicsj974 <dicsj974@gmail.com>
2017-06-28 11:27:17 +0300

    Done expressions and pointers

--- exercises/programming/c/datatypes-expressions/expressions.c
+++ exercises/programming/c/datatypes-expressions/expressions.c
@@ -3,11 +3,26 @@
 int main(int argc, char *argv[])
 {
    // declare integers, floats, and doubles
-
+	int a=200;
+	float b=5.5;
+	double c=1000;
+	
 
    // evaluate expressions, e.g. c = a + 2.5 * b
    // and print out results. Try to combine integers, floats
    // and doubles and investigate what happens in type conversions
+
+	printf("integer a is %d \n", a);
+        printf("float b is %f \n", b);
+	printf("double c is %f \n", c);
+	
+	a=+b;
+	printf("a plus b is %d \n", a);
+	
+	a=+c;
+	printf("a plus c is %d \n", a);
+	printf("a plus c is %f \n", a);
+
 
    return 0;
 }
--- exercises/programming/c/datatypes-expressions/pointers.c
+++ exercises/programming/c/datatypes-expressions/pointers.c
@@ -4,10 +4,34 @@ int main(int argc, char *argv[])
 {
    int a, b, c;
    // declare a pointer variable and assign it to address of e.g. a
-   
+	//Declaring pointer p to variable a
+	int *p;
+	a=15;
+//Pointers to integer
+ /*
+	p=&a; 
    // Evaluate expressions using both the original and the pointer variable
    // and investigate the value / value pointed to
+	printf("a equals %d \n", a);
+	printf("p equals %d \n", p);
+	printf("pointer p adress equals %p \n", p);
 
+//	p++;	
+	printf("p equals %d \n", p);
+	printf("pointer p adress equals %p \n", p);
+	*p=150;
+	printf("*p changed to 150\n");
+	printf("a equals %d \n", a);
+	printf("pointer p equals %d \n", *p);
+	printf("p equals %d \n", p);
+	printf("pointer p adress equals %p \n", p);
+ */
+//Pointers to doubles
+	b=11e23;	
+	p=&a;
+	printf("b equals %d \n", b);
+	printf("p equals %d \n", p);
+	
 
    return 0;
 }
