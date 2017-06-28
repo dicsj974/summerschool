865c6a4edaa1c64c79e95a605ee9aefbd6371c5c
dicsj974 <dicsj974@gmail.com>
2017-06-28 18:51:30 +0300

    fixed 2d_array.c

--- exercises/programming/c/datastructures-functions-heat/2d_array.c
+++ exercises/programming/c/datastructures-functions-heat/2d_array.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//define NX 258
-//int NX=258; ?
-//define NY 258
+#define NX 258
+#define NY 258
 
 
 int main(void)
@@ -11,29 +10,35 @@ int main(void)
     int i, j;
     // Add here the definition for array with name 'array'
     // of size NX times NY (258x258)
-	double array[258][258];
+	double array[NX][NY];
 //#error Define the array
 
     // Initialize first the zeros
 //error Initialize the zeros for interior
-for (i=0; i<258; i++)
-	for (j=0;j<258; j++)
-		array[i][j]=0;
+for (i=0; i<NX - 1; i++){
+	for (j=0;j<NY - 1; j++){
+		array[i][j]=0.0;
+	}
+}
 
     // Initial conditions for left and right
-	
+for (i=0; i< NX;i++){
+	array[i][0] = 20.0;
+	array[i][NY-1] = 70.0;
+}
 
-	//Left
-//for (i=0; i<258; i++)
+	// Initial conditions for top and bottom
+for (j=0; j<NY; j++) {
+	array[0][j]=85.0;
+	array[NX-1][j]=5.0;
+}
 	// Right
 	// Top
 	// Bottom
 
-    // and top and bottom boundaries
-//error add boundary conditions for top and bottom
-for (i=0; i<258; i++){
+for (i=0; i<NX; i++){
 	printf("\n");
-	for (j=0;j<258; j++)
+	for (j=0;j<NY; j++)
 		printf("%f ", array[i][j]);
 }
     return 0;
