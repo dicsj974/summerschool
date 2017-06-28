0f1aee74675fed14dbbd39bb8da5531ee39c9089
dicsj974 <dicsj974@gmail.com>
2017-06-28 18:26:54 +0300

    added c-programming wedn. evening

--- exercises/programming/c/control-structures/my_pow_prog.c
+++ exercises/programming/c/control-structures/my_pow_prog.c
@@ -1,21 +1,25 @@
 /* my_pow.c -- Print out powers of k: 1, k, k^2, k^3, .. up to k^N */
 
 #include <stdio.h>
-
+#include <math.h>  /*  good with for example printf("%g\n", sqrt(20.0));     */
 int main(void)
 {
     int N = 16;
     int k = 2;
 
-    int n;                      /* The current exponent */
+    int n=0;                      /* The current exponent */
     int val = 1;                /* The current power of k  */
 
     printf("\t  n  \t    2^n\n");
     printf("\t================\n");
-
+while (n<N){
+	printf("\t%d\t%d\n",n, pow(2, (val-1)));
+	n++;
+	val++;
+}
     /* Add here a for loop that at each iteration prints out
        the current value of the power and updates val */
-#error Add the missing loop
+//#error Add the missing loop
 
     return 0;
 }
--- exercises/programming/c/datastructures-functions-heat/2d_array.c
+++ exercises/programming/c/datastructures-functions-heat/2d_array.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NX 258
-#define NY 258
+//define NX 258
+//int NX=258; ?
+//define NY 258
+
 
 int main(void)
 {
     int i, j;
     // Add here the definition for array with name 'array'
     // of size NX times NY (258x258)
-#error Define the array
+	double array[258][258];
+//#error Define the array
 
     // Initialize first the zeros
-#error Initialize the zeros for interior
+//error Initialize the zeros for interior
+for (i=0; i<258; i++)
+	for (j=0;j<258; j++)
+		array[i][j]=0;
 
     // Initial conditions for left and right
-#error add boundary conditions for left and right
+	
+
+	//Left
+//for (i=0; i<258; i++)
+	// Right
+	// Top
+	// Bottom
 
     // and top and bottom boundaries
-#error add boundary conditions for top and bottom
-
+//error add boundary conditions for top and bottom
+for (i=0; i<258; i++){
+	printf("\n");
+	for (j=0;j<258; j++)
+		printf("%f ", array[i][j]);
+}
     return 0;
 }
