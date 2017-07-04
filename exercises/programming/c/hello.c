e8ddf7cfb208abeaa866342199daf1505c6b0668
dicsj974 <dicsj974@gmail.com>
2017-07-04 15:39:09 +0300

    Adding files forgotten to add.

---
+++ exercises/programming/c/hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int a;
+	int *p;
+
+	a=6;
+	p=&a;
+	
+//	*p=8
+
+printf("a is %d\n", a);
+printf("p is %d\n", p);
+
+printf("value pointed by p is %p\n", p);
+
+
+}
+
